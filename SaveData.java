183bed9c041ded4682a98054204d5387e34cd346
wafaa-hub <60840675+wafaa-hub@users.noreply.github.com>
2020-02-10 06:38:17 -0800

    Add files via upload

---
+++ SaveData.java
@@ -0,0 +1,492 @@
+package org.example;
+
+public class SaveData {
+
+    public SaveData(String date, String askprice, String gclose, String change, String gvolume, String gesi, String last, String dirction, String bidprice, String egi, String gpchange, String lastvolume, String tbv, String open, String tav, String askvolume, String outflowers, String gchange, String volume, String time, String week52low, String inflowvalue, String week52high, String wsi, String trades, String ipchange, String gegi, String min, String topic, String pchange, String esi, String max, String issnapshot, String wgi, String pclose, String maxlv, String iclose, String outflowvolume, String lastvalue, String high, String low, String lasttradeprice, String phigh, String outfloworders) {
+        this.date = date;
+        this.askprice = askprice;
+        this.gclose = gclose;
+        this.change = change;
+        this.gvolume = gvolume;
+        this.gesi = gesi;
+        this.last = last;
+        this.dirction = dirction;
+        this.bidprice = bidprice;
+        this.egi = egi;
+        this.gpchange = gpchange;
+        this.lastvolume = lastvolume;
+        this.tbv = tbv;
+        this.open = open;
+        this.tav = tav;
+        this.askvolume = askvolume;
+        this.outflowers = outflowers;
+        this.gchange = gchange;
+        this.volume = volume;
+        this.time = time;
+        this.week52low = week52low;
+        this.inflowvalue = inflowvalue;
+        this.week52high = week52high;
+        this.wsi = wsi;
+        this.trades = trades;
+        this.ipchange = ipchange;
+        this.gegi = gegi;
+        this.min = min;
+        this.topic = topic;
+        this.pchange = pchange;
+        this.esi = esi;
+        this.max = max;
+        this.issnapshot = issnapshot;
+        this.wgi = wgi;
+        this.pclose = pclose;
+        this.maxlv = maxlv;
+        this.iclose = iclose;
+        this.outflowvolume = outflowvolume;
+        this.lastvalue = lastvalue;
+        this.high = high;
+        this.low = low;
+        this.lasttradeprice = lasttradeprice;
+        this.phigh = phigh;
+        this.outfloworders = outfloworders;
+    }
+
+    String date;
+    String gclose;
+    public String getAskprice() {
+        return askprice;
+    }
+    String askprice;
+
+    public void setAskprice(String askprice) {
+        this.askprice = askprice;
+    }
+
+
+    public String getGclose() {
+        return gclose;
+    }
+
+    public void setGclose(String gclos) {
+        this.gclose = gclose;
+    }
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+    String gclos;
+
+    public String getChange() {
+        return change;
+    }
+
+    public void setChange(String change) {
+        this.change = change;
+    }
+
+    String change;
+
+    public String getGvolume() {
+        return gvolume;
+    }
+
+    public void setGvolume(String gvolume) {
+        this.gvolume = gvolume;
+    }
+
+    String gvolume;
+
+    public String getGesi() {
+        return gesi;
+    }
+
+    public void setGesi(String gesi) {
+        this.gesi = gesi;
+    }
+
+    String gesi;
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    String last;
+
+    public String getDirction() {
+        return dirction;
+    }
+
+    public void setDirction(String dirction) {
+        this.dirction = dirction;
+    }
+
+    String dirction;
+
+    public String getBidprice() {
+        return bidprice;
+    }
+
+    public void setBidprice(String bidprice) {
+        this.bidprice = bidprice;
+    }
+
+    String bidprice;
+
+    public String getEgi() {
+        return egi;
+    }
+
+    public void setEgi(String egi) {
+        this.egi = egi;
+    }
+
+    String egi;
+
+    public String getGpchange() {
+        return gpchange;
+    }
+
+    public void setGpchange(String gpchange) {
+        this.gpchange = gpchange;
+    }
+
+    String gpchange;
+
+    public String getLastvolume() {
+        return lastvolume;
+    }
+
+    public void setLastvolume(String lastvolume) {
+        this.lastvolume = lastvolume;
+    }
+
+    String lastvolume;
+
+    public String getTbv() {
+        return tbv;
+    }
+
+    public void setTbv(String tbv) {
+        this.tbv = tbv;
+    }
+
+    String tbv;
+
+    public String getOpen() {
+        return open;
+    }
+
+    public void setOpen(String open) {
+        this.open = open;
+    }
+
+    String open;
+
+    public String getTav() {
+        return tav;
+    }
+
+    public void setTav(String tav) {
+        this.tav = tav;
+    }
+
+    String tav;
+
+    public String getAskvolume() {
+        return askvolume;
+    }
+
+    public void setAskvolume(String askvolume) {
+        this.askvolume = askvolume;
+    }
+
+    String askvolume;
+
+    public String getOutflowers() {
+        return outflowers;
+    }
+
+    public void setOutflowers(String outflowers) {
+        this.outflowers = outflowers;
+    }
+
+    String outflowers;
+
+    public String getGchange() {
+        return gchange;
+    }
+
+    public void setGchange(String gchange) {
+        this.gchange = gchange;
+    }
+
+    String gchange;
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    String volume;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    String time;
+
+    public String getWeek52low() {
+        return week52low;
+    }
+
+    public void setWeek52low(String week52low) {
+        this.week52low = week52low;
+    }
+
+    String week52low;
+
+    public String getInflowvalue() {
+        return inflowvalue;
+    }
+
+    public void setInflowvalue(String inflowvalue) {
+        this.inflowvalue = inflowvalue;
+    }
+
+    String inflowvalue;
+
+    public String getWeek52high() {
+        return week52high;
+    }
+
+    public void setWeek52high(String week52high) {
+        this.week52high = week52high;
+    }
+
+    String week52high;
+
+    public String getWsi() {
+        return wsi;
+    }
+
+    public void setWsi(String wsi) {
+        this.wsi = wsi;
+    }
+
+    String wsi;
+
+    public String getTrades() {
+        return trades;
+    }
+
+    public void setTrades(String trades) {
+        this.trades = trades;
+    }
+
+    String trades;
+
+    public String getIpchange() {
+        return ipchange;
+    }
+
+    public void setIpchange(String ipchange) {
+        this.ipchange = ipchange;
+    }
+
+    String ipchange;
+
+    public String getGegi() {
+        return gegi;
+    }
+
+    public void setGegi(String gegi) {
+        this.gegi = gegi;
+    }
+
+    String gegi;
+
+    public String getMin() {
+        return min;
+    }
+
+    public void setMin(String min) {
+        this.min = min;
+    }
+
+    String min;
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    String topic;
+
+    public String getPchange() {
+        return pchange;
+    }
+
+    public void setPchange(String pchange) {
+        this.pchange = pchange;
+    }
+
+    String pchange;
+
+    public String getEsi() {
+        return esi;
+    }
+
+    public void setEsi(String esi) {
+        this.esi = esi;
+    }
+
+    String esi;
+
+    public String getMax() {
+        return max;
+    }
+
+    public void setMax(String max) {
+        this.max = max;
+    }
+
+    String max;
+
+    public String getIssnapshot() {
+        return issnapshot;
+    }
+
+    public void setIssnapshot(String issnapshot) {
+        this.issnapshot = issnapshot;
+    }
+
+    String issnapshot;
+
+    public String getWgi() {
+        return wgi;
+    }
+
+    public void setWgi(String wgi) {
+        this.wgi = wgi;
+    }
+
+    String wgi;
+
+    public String getPclose() {
+        return pclose;
+    }
+
+    public void setPclose(String pclose) {
+        this.pclose = pclose;
+    }
+
+    String pclose;
+
+    public String getMaxlv() {
+        return maxlv;
+    }
+
+    public void setMaxlv(String maxlv) {
+        this.maxlv = maxlv;
+    }
+
+    String maxlv;
+
+    public String getIclose() {
+        return iclose;
+    }
+
+    public void setIclose(String iclose) {
+        this.iclose = iclose;
+    }
+
+    String iclose;
+
+    public String getOutflowvolume() {
+        return outflowvolume;
+    }
+
+    public void setOutflowvolume(String outflowvolume) {
+        this.outflowvolume = outflowvolume;
+    }
+
+    String outflowvolume;
+
+    public String getLastvalue() {
+        return lastvalue;
+    }
+
+    public void setLastvalue(String lastvalue) {
+        this.lastvalue = lastvalue;
+    }
+
+    String lastvalue;
+
+    public String getHigh() {
+        return high;
+    }
+
+    public void setHigh(String high) {
+        this.high = high;
+    }
+
+    String high;
+
+    public String getLow() {
+        return low;
+    }
+
+    public void setLow(String low) {
+        this.low = low;
+    }
+
+    String low;
+
+    public String getLasttradeprice() {
+        return lasttradeprice;
+    }
+
+    public void setLasttradeprice(String lasttradeprice) {
+        this.lasttradeprice = lasttradeprice;
+    }
+
+    String lasttradeprice;
+
+    public String getPhigh() {
+        return phigh;
+    }
+
+    public void setPhigh(String phigh) {
+        this.phigh = phigh;
+    }
+
+    String phigh;
+
+    public String getOutfloworders() {
+        return outfloworders;
+    }
+
+    public void setOutfloworders(String outfloworders) {
+        this.outfloworders = outfloworders;
+    }
+
+    String outfloworders;
+
+
+}
+
