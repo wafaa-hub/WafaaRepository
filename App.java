20364fde7c258c57f3b377f66d72e14c17e7ca7e
wafaa-hub <60840675+wafaa-hub@users.noreply.github.com>
2020-02-11 07:52:01 -0800

    Update App.java

--- App.java
+++ App.java
@@ -7,7 +7,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Iterator;
 
 public class App
 {
@@ -26,15 +25,156 @@ public class App
 
              @Override
              public void onMessage(String s) {
-                 JSONObject jsonObject = new JSONObject(s);
 
-                 try {
-                   // for example
-                   updateJson(jsonObject,"max","123456");
+                JSONObject jsonObject = new JSONObject(s);
 
+                Map<String,Object> map = new HashMap<>();
+                map.put(jsonObject.getString("topic"), new Company(jsonObject));
+                String newVal ="987987";
+                try {
+
+                if (map.containsKey("topic")) {
+
+                    switch(jsonObject.keys().toString())
+                    {
+                        case "date" :
+                            new Company().update("date", newVal);
+                            break;
+                        case "askprice" :
+                            new Company().update("askprice", newVal);
+                            break;
+                        case "gclose" :
+                            new Company().update("gclose", newVal);
+                            break;
+                        case "change" :
+                            new Company().update("change", newVal);
+                            break;
+                        case "gesi" :
+                            new Company().update("gesi", newVal);
+                            break;
+                        case "last" :
+                            new Company().update("last", newVal);
+                            break;
+                        case "direction" :
+                            new Company().update("direction", newVal);
+                            break;
+                        case "bidprice" :
+                            new Company().update("bidprice", newVal);
+                            break;
+                        case "egi" :
+                            new Company().update("egi", newVal);
+                            break;
+                        case "gpchange" :
+                            new Company().update("gpchange", newVal);
+                            break;
+                        case "gvolume" :
+                            new Company().update("gvolume", newVal);
+                            break;
+                        case "tbv" :
+                            new Company().update(" tbv", newVal);
+                            break;
+                        case "open" :
+                            new Company().update("open", newVal);
+                            break;
+                        case "tav" :
+                            new Company().update("tav", newVal);
+                            break;
+                        case "askvolume" :
+                            new Company().update("askvolume", newVal);
+                            break;
+                        case "outflowers" :
+                            new Company().update("outflowers", newVal);
+                            break;
+                        case "gchange" :
+                            new Company().update("gchange", newVal);
+                            break;
+                        case "volume" :
+                            new Company().update("volume", newVal);
+                            break;
+                        case "time" :
+                            new Company().update("time", newVal);
+                            break;
+                        case "week52low" :
+                            new Company().update("week52low", newVal);
+                            break;
+                        case "inflowvalue" :
+                            new Company().update("inflowvalue", newVal);
+                            break;
+                        case "week52high" :
+                            new Company().update("week52high", newVal);
+                            break;
+                        case "wsi" :
+                            new Company().update("wsi", newVal);
+                            break;
+                        case "trades" :
+                            new Company().update("trades", newVal);
+                            break;
+                        case "ipchange" :
+                            new Company().update("ipchange", newVal);
+                            break;
+                        case "gegi" :
+                            new Company().update("gegi", newVal);
+                            break;
+                        case "min" :
+                            new Company().update("min", newVal);
+                            break;
+                        case "pchange" :
+                            new Company().update("pchange", newVal);
+                            break;
+                        case "esi" :
+                            new Company().update("esi", newVal);
+                            break;
+                        case "max" :
+                            new Company().update("max", newVal);
+                            break;
+                        case "issnapshot" :
+                            new Company().update("issnapshot", newVal);
+                            break;
+                        case "wgi" :
+                            new Company().update("wgi", newVal);
+                            break;
+                        case "pclose" :
+                            new Company().update("pclose", newVal);
+                            break;
+                        case "maxlv" :
+                            new Company().update("maxlv", newVal);
+                            break;
+                        case "iclose" :
+                            new Company().update("iclose", newVal);
+                            break;
+                        case "outflowvolume" :
+                            new Company().update("outflowvolume", newVal);
+                            break;
+                        case "lastvalue" :
+                            new Company().update("lastvalue", newVal);
+                            break;
+                        case "high" :
+                            new Company().update("high", newVal);
+                            break;
+                        case "low" :
+                            new Company().update("low", newVal);
+                            break;
+                        case "lasttradeprice" :
+                            new Company().update("lasttradeprice", newVal);
+                            break;
+                        case "phigh" :
+                            new Company().update("phigh", newVal);
+                            break;
+                        case "outfloworders" :
+                            new Company().update("outfloworders", newVal);
+                            break;
+
+                    }
+
+                }
+                else {
+                    map.put(jsonObject.getString("topic"), new Company(jsonObject));
+
+                }
                  } catch (Exception e) {
                      e.printStackTrace();
                  }
+
              }
 
                 @Override
@@ -47,30 +187,10 @@ public class App
 
                 }
             };
-
                 webSocket.connect();
+
         }
 
-    public static JSONObject updateJson(JSONObject obj, String keyString, String newValue) throws Exception {
 
-        Map<String, String> map = new HashMap<String, String>();
-        Iterator iterator = obj.keys();
-        String key = null, value = null;
-        while (iterator.hasNext()) {
-            key = (String) iterator.next();
-            value = obj.getString(key);
-            map.put(key,value);
-            if ( (obj.optJSONObject(key) == null)) {
-                if ((key.equals(keyString))) {
-                    obj.put(key, newValue);
-                    return obj;
-                }
-            }
-            if (obj.optJSONObject(key) != null) {
-                updateJson(obj.getJSONObject(key), keyString, newValue);
-            }
-        }
-        return obj;
-    }
 
 }
