785c0b64c8d54a300d7f1cfbd81fa9e9fc8029bd
wafaa-hub <60840675+wafaa-hub@users.noreply.github.com>
2020-02-16 07:23:36 -0800

    Update App.java

--- App.java
+++ App.java
@@ -22,15 +22,12 @@ public class App {
                 this.send("uid=034d4dc7-a9bc-412e-9051-c32605336934");
                 this.send("subscribe=QO.2010.TAD,QO.4180.TAD,QO.4061.TAD,QO.2140.TAD,QO.4130.TAD,QO.6070.TAD,QO.1120.TAD,QO.2170.TAD,QO.1080.TAD,QO.3010.TAD,QO.2210.TAD,QO.2200.TAD,QO.4150.TAD,QO.4080.TAD,QO.6060.TAD,QO.1020.TAD,QO.1050.TAD,QO.3080.TAD,QO.2180.TAD,QO.2100.TAD,QO.6090.TAD,QO.4190.TAD,QO.4100.TAD,QO.6010.TAD,QO.4160.TAD,QO.2080.TAD,QO.2090.TAD,QO.2060.TAD,QO.2220.TAD,QO.6020.TAD,QO.1010.TAD,QO.1090.TAD,QO.2120.TAD,QO.2020.TAD,QO.2030.TAD,QO.2160.TAD,QO.4050.TAD,QO.2110.TAD,QO.3030.TAD,QO.2040.TAD,QO.2230.TAD,QO.5110.TAD,QO.6050.TAD,QO.4010.TAD,QO.2130.TAD,QO.4140.TAD,QO.2250.TAD,QO.2190.TAD,QO.4110.TAD,QO.2070.TAD");
                 System.out.println("socket is open....");
-                //System.out.println("The top Gamers Companies Asc Order: \n");
-
             }
 
             @Override
             public void onMessage(String s) {
 
                 JSONObject jsonObject = new JSONObject(s);
-
                 try {
                     if (map.containsKey(jsonObject.getString("topic"))) {
                         map.get(jsonObject.getString("topic")).update(jsonObject);
@@ -41,27 +38,18 @@ public class App {
                     e.printStackTrace();
                 }
 
-                // System.out.println(map.get("QO.6050.TAD").toString()); // to print data of company
+                List<Map.Entry<String, Company>> list = new ArrayList<>(map.entrySet());
+                
 
-                List<Company> companies = new ArrayList<>();
-                Iterator<?> keys = jsonObject.keys();
-                Company company;
-                while (keys.hasNext()) {
-                    String key = (String) keys.next();
-                    company = new Company(key, jsonObject.optInt(key));
-                    companies.add(company);
-                }
-
-                Collections.sort(companies, new Comparator<>() {
+               Collections.sort(list, new Comparator<Map.Entry<String, Company>>() {
                     @Override
-                    public int compare(Company company1, Company company2) {
-                        return company1.change.compareTo(company2.change);
+                    public int compare(Map.Entry<String, Company> t, Map.Entry<String, Company> t1) {
+                        return t.getValue().change.compareTo(t1.getValue().change);
+
                     }
                 });
 
-                System.out.println("The Top Gamers Companies : \n");
-                for (int i = 0; i < companies.size(); i++)
-                    System.out.println(companies.get(i));
+               // System.out.println(list);
 
             }
 
